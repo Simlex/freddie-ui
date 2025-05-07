de89a485ba8e8cf6df6e2a6eab7df1e6978f5c97
Simlex <simlexafol@gmail.com>
2025-05-07 07:49:33 +0100

    feat: wrap GlobalProvider with ManagerProvider and IdentityProvider

--- components/shared/Provider.tsx
+++ components/shared/Provider.tsx
@@ -1,13 +1,21 @@
 "use client";
 import React from "react";
 import { AppProvider } from "@/contexts/ApplicationContext";
+import { ManagerProvider } from "@/contexts/ManagerContext";
+import { IdentityProvider } from "@/contexts/IdentitiesContext";
 
 type Props = {
   children?: React.ReactNode;
 };
 
 const GlobalProvider = ({ children }: Props) => {
-  return <AppProvider>{children}</AppProvider>;
+  return (
+    <AppProvider>
+      <ManagerProvider>
+        <IdentityProvider>{children}</IdentityProvider>
+      </ManagerProvider>
+    </AppProvider>
+  );
 };
 
 export default GlobalProvider;

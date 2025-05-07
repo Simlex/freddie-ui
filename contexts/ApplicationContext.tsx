0c9c70bb209c4fe0308d25ed1acaa2088a215caf
Simlex <simlexafol@gmail.com>
2025-05-07 02:33:47 +0100

    refactor: improve formatting and readability of ApplicationContext.tsx

--- contexts/ApplicationContext.tsx
+++ contexts/ApplicationContext.tsx
@@ -1,52 +1,65 @@
-import { Dispatch, FunctionComponent, ReactNode, SetStateAction, createContext, useContext, useEffect, useState } from "react";
+import {
+  Dispatch,
+  FunctionComponent,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { useOnline } from "../hooks/useOnline";
+import { Manager } from "@/models/IManager";
 
 // Define the type for the context data
 export type ApplicationContextData = {
-    offlineStatusModalVisibility: boolean;
-    hideOfflineStatusModalVisibility: Dispatch<SetStateAction<boolean>>
+  offlineStatusModalVisibility: boolean;
+  hideOfflineStatusModalVisibility: Dispatch<SetStateAction<boolean>>;
 };
 
 // Create a context with the specified data type
-const ApplicationContext = createContext<ApplicationContextData | undefined>(undefined);
+const ApplicationContext = createContext<ApplicationContextData | undefined>(
+  undefined
+);
 
 // Create a provider component that takes children as props
 type AppProviderProps = {
-    children: ReactNode;
+  children: ReactNode;
 };
 
 const AppProvider: FunctionComponent<AppProviderProps> = ({ children }) => {
-    const [offlineStatusModalVisibility, setOfflineStatusModalVisibility] = useState(false);
+  const [offlineStatusModalVisibility, setOfflineStatusModalVisibility] =
+    useState(false);
 
-    const isOnline = useOnline();
-    
-    useEffect(() => {
-        if (!isOnline) {
-            setOfflineStatusModalVisibility(true);
-        } else {
-            setOfflineStatusModalVisibility(false);
-        }
-    }, [isOnline])
+  const isOnline = useOnline();
 
-    // Define the values you want to share
-    const sharedData: ApplicationContextData = {
-        offlineStatusModalVisibility,
-        hideOfflineStatusModalVisibility: setOfflineStatusModalVisibility
-    };
+  useEffect(() => {
+    if (!isOnline) {
+      setOfflineStatusModalVisibility(true);
+    } else {
+      setOfflineStatusModalVisibility(false);
+    }
+  }, [isOnline]);
 
-    return (
-        <ApplicationContext.Provider value={sharedData}>
-            {children}
-        </ApplicationContext.Provider>
-    );
+  // Define the values you want to share
+  const sharedData: ApplicationContextData = {
+    offlineStatusModalVisibility,
+    hideOfflineStatusModalVisibility: setOfflineStatusModalVisibility,
+  };
+
+  return (
+    <ApplicationContext.Provider value={sharedData}>
+      {children}
+    </ApplicationContext.Provider>
+  );
 };
 
 export { AppProvider, ApplicationContext };
 
 export const useApplicationContext = () => {
-    const context = useContext(ApplicationContext);
-    if (context === undefined) {
-        throw new Error("useApplicationContext must be used within a AppProvider");
-    }
-    return context;
-}
+  const context = useContext(ApplicationContext);
+  if (context === undefined) {
+    throw new Error("useApplicationContext must be used within a AppProvider");
+  }
+  return context;
+};

87a4597d132e2a3ddce6f21ea6840c3167863bba
Simlex <simlexafol@gmail.com>
2025-05-07 07:44:58 +0100

    refactor: clean up IManager.ts by removing unused types and improving clarity

--- models/IManager.ts
+++ models/IManager.ts
@@ -1,3 +1,5 @@
+import { Identity } from "./IIdentity";
+
 export type Manager = {
   id: string;
   fullName: string;
@@ -6,6 +8,11 @@ export type Manager = {
   createdAt: string;
 };
 export type ManagerCreate = Omit<Manager, "id" | "createdAt"> & {
-    email: string;
+  email: string;
 };
 export type ManagerUpdate = Partial<Omit<Manager, "createdAt">>;
+
+export type ManagerIdentity = {
+  identities: Identity[];
+  count: number;
+};
